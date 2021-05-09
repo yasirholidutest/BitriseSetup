cfbaa63d807dc44c35e5b73f0117a6d16706cfde
mohammadyasirperwez <yasir.perwez@holidu.com>
2021-05-09 16:07:04 +0200

    test commit 14

--- BitriseKit/Tests/BitriseKitTests/BitriseKitTests.swift
+++ BitriseKit/Tests/BitriseKitTests/BitriseKitTests.swift
@@ -16,7 +16,7 @@
       }
       
       
-      func testExampleAAA() {
+      func testExampleAAAX() {
           // This is an example of a functional test case.
           // Use XCTAssert and related functions to verify your tests produce the correct
           // results.
